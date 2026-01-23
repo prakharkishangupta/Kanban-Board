d08de277f94eafdec99079d026465297a29e9e7f
Prakhar Kumar Gupta <prakharkumargupta@Prakhars-MacBook-Air.local>
2026-01-23 16:07:05 +0530

    error 401

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get("/", getTasks);
-router.post("/", authMiddleware, createTask);
+router.post("/", createTask);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
 router.patch("/drag", dragTask);
